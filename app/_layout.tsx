600ea4dee5e73c5b169823cddfae4096434324ff
Pierlee <pierleefranklin@gmail.com>
2025-05-22 03:49:58 -0400

    installed react native paper design system

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,10 @@
 import { Stack } from "expo-router";
+import { PaperProvider, MD3DarkTheme } from "react-native-paper";
 
 export default function Layout() {
-  return <Stack />;
+  return (
+    <PaperProvider theme={MD3DarkTheme}>
+      <Stack />
+    </PaperProvider>
+  );
 }
